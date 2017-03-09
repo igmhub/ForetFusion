6ba164f6824fe5e6ed905c8e14f8230686bb38d7
Anze Slosar <slosar@gmail.com>
2017-03-09 09:03:24 -0500

    start
    Former-commit-id: 3f626a54a74dd555cd03af672e95a94ba3516dd1

--- ffusion/get_files.py
+++ ffusion/get_files.py
@@ -3,19 +3,8 @@
 import os, sys
 import fitsio
 import numpy as np
-import pandas as pd
 
-def read_sub_fits(dir_fits, file_name):
-    """Read the subsample of spAll we are interested in. Return a DataFrame"""
-    file_name = os.path.join(dir_fits, file_name)
-    if not os.path.isfile(file_name):
-        print ('File not found: {}'.format(file_name))
-        #MPI.COMM_WORLD.Abort()
-        sys.exit(1)
 
-    print ('Reading file...')
-    df = pd.read_csv(file_name, sep=',')
-    return df
 
 
 
@@ -31,13 +20,7 @@ def read_fits(dir_fits, file_name, fits_cols, maxnum=0):
     #http://stackoverflow.com/questions/30283836/
     # creating-pandas-dataframe-from-numpy-array-leads-to-strange-errors
     fits_read  = fits[1].read(columns= fits_cols)
-    fits_to_df = {col:fits_read[col].byteswap().newbyteorder() for col in fits_cols}
-    df = pd.DataFrame(fits_to_df)
-    if (maxnum>0):
-        df=df[:maxnum]
-    print "Read",maxnum,"entries from ",file_name
-
-    return df
+    return fits_read
 
 
 def read_spframe(dir_fits, triplet):
@@ -47,49 +30,13 @@ def read_spframe(dir_fits, triplet):
         print('File not found: {}'.format(fname))
         sys.exit(1)
     fits       = fitsio.FITS(fname)
-    #http://stackoverflow.com/questions/30283836/
-    # creating-pandas-dataframe-from-numpy-array-leads-to-strange-errors
     flux = fits[1].read()[fiber-1]
     ivar = fits[2].read()[fiber-1]
     andmask = fits[3].read()[fiber-1]
     ormask = fits[4].read()[fiber-1]
     head=fitsio.read_header(fname)
     loglam = head['COEFF0']+head['COEFF1']*np.arange(len(flux))
-    fits_to_df = {'flux':flux, 'ivar':ivar,'and_mask':andmask,'or_mask':ormask,'loglam':loglam}
-    df = pd.DataFrame(fits_to_df)
+    dtype=[('lam','f4'),('flux','f4'),('ivar','f4'),('andmask','i4')]
     return df
-
-
-
-if False:
-    #Just In case we need to get the spec files from the sdss-website
-    #we need import mechanize and from base64 import b64encode
-    def get_web_files(self, file_name, passwd):
-        """nasty hack to get files from sdss website, but will change it later,
-            only works with Python2"""
-
-        self.sdss_url    = 'https://data.sdss.org/sas/ebosswork/eboss/spectro/redux/v5_10_0/spectra/'
-        if self.verbose: print ('Getting file {} from the sdss web'.format(file_name))
-        url = os.path.join(self.sdss_url, file_name)
-        username = 'sdss'
-        password = '{}'.format(passwd)
-
-        # I have had to add a carriage return ('%s:%s\n'), but
-        # you may not have to.
-        b64login = b64encode('%s:%s' % (username, password))
-
-        br = mechanize.Browser()
-        br.set_handle_robots(False)
-        br.addheaders.append(
-          ('Authorization', 'Basic %s' % b64login )
-        )
-        br.open(url)
-        r = br.response()
-        data = r.read()
-
-        with open(os.path.join(self.dir_spec, file_name),'wb') as output:
-              output.write(data)
-
-        return 0
 
 

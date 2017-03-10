e50c842231cd1ddc947484368782ec63195d34b7
Anze Slosar <slosar@gmail.com>
2017-03-10 15:21:53 -0500

    missing file
    Former-commit-id: 424cb3ca34bd7091bb83590f5f4c316b2fa4e303

---
+++ ffusion/settings.py
@@ -0,0 +1,16 @@
+#
+# Settings module
+#
+
+useMPI,comm,rank,size = False, None, 0, 1
+rootdir="."
+DRQ="/global/projecta/projectdirs/sdss/eBOSS/DR14Q_v1_1.fits"
+#spPlate_dir= '/global/projecta/projectdirs/sdss/staging/ebosswork/eboss/spectro/redux/v5_10_0/'
+spPlate_dir='/global/projecta/projectdirs/sdss/eBOSS/testFiles'
+pix_dir=rootdir+"/pixs"
+maxNobj=10000000
+Nside=8
+logl_min=3.55
+0 #640
+logl_max=4.025
+logl_step=1e-4
